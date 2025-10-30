fbce53cd6388ba8bec14861d20402fd747f0f7e2
nicolas-veiga762 <nicolasveiga24@gmail.com>
2025-10-29 21:57:51 -0300

    teste

--- functions.h
+++ functions.h
@@ -1,24 +1,28 @@
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
 
-#define M 10
-#define N 10
+
+void inicializa_matriz(int m, int n, int matriz[m][n]);
 
 int verifica_indice(int indice, int contador_conjuntos);
-int criar_conjunto_vazio(int contador_conjuntos);
 
-int busca_valor(int conjunto[N], int valor);
+int criar_conjunto_vazio(int contador_conjuntos, int m);
+
+void inserir_valores(int m, int n, int matriz[m][n], int indice, int contador_conjuntos);
+
+void uniao_conjuntos(int m, int n, int matriz[m][n], int indice, int indice2, int contador_conjuntos);
+
+void interseccao_conjuntos(int m, int n, int matriz[m][n], int indice, int indice2, int contador_conjuntos);
+
+void mostrar_conjunto(int m , int n, int matriz[m][n], int indice);
+
+void mostrar_todos_conjuntos(int m, int n, int matriz[m][n], int contador_conjuntos);
+
+
+int busca_valor(int n, int conjunto[], int valor);
+void remover_conjunto(int m, int n, int matriz[m][n], int indice);
+void busca_por_valor(int m, int n, int matriz[m][n], int valor);
 void limpa_tela();
 void pausa();
-void ordenarConjunto(int conjunto[N]);
-
-void inicializa_matriz(int matriz[M][N]);
-void inserir_valores(int matriz[M][N], int indice);
-int remover_conjunto(int matriz[M][N], int indice, int contador_conjuntos);
-void uniao_conjuntos(int matriz[M][N], int indice1, int indice2, int contador_conjuntos);
-void interseccao_conjuntos(int matriz[M][N], int indice1, int indice2, int contador_conjuntos);
-void mostrar_conjunto(int matriz[M][N], int indice);
-void mostrar_todos_conjuntos(int matriz[M][N], int contador_conjuntos);
-void busca_por_valor(int matriz[M][N], int valor, int contador_conjuntos);
 
 #endif
